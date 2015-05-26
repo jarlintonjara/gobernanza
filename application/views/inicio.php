4dbbfa76d021e186d56243b7e9d3bee898c2e76a
alexis <alejarahi@gmail.com>
2015-05-26 17:31:36 -0500

    Iniciando PY_Gobernanza

---
+++ application/views/inicio.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>Gobernanza</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url() ?>assets/css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+   <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+                <h3 class="text-muted">Project name</h3>
+      </div>
+
+       <form class="form-inline"> 
+          <div id="Login">
+              <div class="Box">
+                <div class="User">bg-user</div>
+                    <h2 class="Translate">Usuario</h2>
+                    <input type="text" name="" value="" placeholder="">
+              </div>
+              <div class="Box">
+                <div class="Password">bg-password</div>
+                    <h2 class="Translate">Contraseña</h2>
+                    <input type="password" name="" value="" placeholder="">
+              </div>
+              <div align="center">
+                  <button type="button" class="btn btn-danger btn-lg" style="border-radius: 0px;">Ingresar</button>
+              </div>
+                
+          </div>
+        </form>
+    </div> <!-- /container -->
+<div id="Footer" class="Translate">
+    <div class="container" lang="es-ES" style="">
+        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    </div>
+</div>
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  
+
+        </body>
+    </html>
